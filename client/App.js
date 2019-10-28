fdc247c4a3dc9b6e65f2ac3ae5f2373c1a444836
WebDevIsac <isaclarsson98@gmail.com>
2019-10-28 13:21:30 +0100

    Add apollo-client with prisma server connection

--- client/App.js
+++ client/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+
+import { AUTH_TOKEN } from "./constants";
+import { InMemoryCache } from "apollo-boost";
 
 const App = () => {
-  return (
-    <ApolloProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    </ApolloProvider>
-  );
-}
+
+	const httpLink = createHttpLink({
+		uri: "http://localhost:4000"
+	});
+
+	const authLink = setContext((trash, { headers }) => {
+		const token = localStorage.getItem(AUTH_TOKEN);
+
+		return {
+			headers: {
+				...headers,
+				authorization: token ? `Bearer ${token}` : ""
+			}
+		}
+	});
+
+	const link = authLink.concat(httpLink);
+
+	const client = new ApolloClient({
+		link,
+		cache: new InMemoryCache()
+	});
+
+
+	return (
+		<ApolloProvider client={client}>
+			<View style={styles.container}>
+				<Text>Hello</Text>
+			</View>
+		</ApolloProvider>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
 
 export default App;
