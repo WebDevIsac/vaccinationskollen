1fe6a07e46d0fb3113c47f831daf32c3fbc6ee3a
WebDevIsac <isaclarsson98@gmail.com>
2019-10-31 12:48:33 +0100

    Add query to get all users vaccinations on server

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,10 +1,30 @@
 const { getUserId } = require("../utils");
 
-function getUser(parent, args, context, info) {
+const getUser = (parent, args, context, info) => {
 	const userId = getUserId(context);
 	return context.prisma.user({ id: userId });
 }
 
+const getVaccinations = (parent, args, context, info) => {
+	return context.prisma.vaccinations();
+}
+
+const getUserVaccinations = async (parent, args, context, info) => {
+	const userId = getUserId(context);
+
+	const vaccinations = await context.prisma.userVaccinations({where: { user: { id: userId } } }).type();
+	let userVaccinations = await context.prisma.userVaccinations({where: { user: { id: userId } } });
+	userVaccinations = userVaccinations.map((userVacc, index) => {
+		userVacc.type = vaccinations[index].type;
+		return userVacc;
+	})
+
+	return userVaccinations;
+}
+
+
 module.exports = {
-	getUser
+	getUser,
+	getVaccinations,
+	getUserVaccinations
 }
